463576a50672870a85ab6b34675e9491c4331f53
TangoMajom <tudomtom@gmail.com>
2018-08-29 10:44:46 +0200

    PHPRuntimeHandler fixed to not restrict the type of expection

--- src/Core/ErrorHandlers/PHPRuntimeHandler.php
+++ src/Core/ErrorHandlers/PHPRuntimeHandler.php
@@ -14,8 +14,8 @@ use Slim\Http\Response;
 
 class PHPRuntimeHandler
 {
-    public function __invoke(Request $request, Response $response,\Exception $exception) {
-        var_dump($exception);
+    public function __invoke(Request $request, Response $response, $exception) {
+      
         return $response
             ->withStatus(500)
             ->withHeader('Content-Type', 'application/json')
