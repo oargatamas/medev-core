fc3a9ff05e5a11094f93dd868b61de774e6ae450
Tangomajom <kutyafika>
2019-01-26 16:51:15 +0100

    Constant keys moved to unique class

---
+++ src/Core/Action/RequestAttribute.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: OargaTamas
+ * Date: 2019. 01. 26.
+ * Time: 16:38
+ */
+
+namespace MedevSlim\Core\Action;
+
+
+class RequestAttribute
+{
+    const IP_ADDRESS = "ip_address";
+    const SCOPES = "scopes";
+    const CORRELATION_ID = "correlation_id";
+    const HANDLER_SERVICE = "handler_service";
+    const RESPONSE_DATA = "response_data";
+}
