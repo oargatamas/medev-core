9d9676314b1af3e6fea531e347c84967d6a4a2fc
Tangomajom <kutyafika>
2019-01-27 12:55:20 +0100

    MedevDatabase class has been added but we are just using the class for the DIC component key. Database implementation and registration may differ in the apps.

---
+++ src/Core/Database/MedevDatabase.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: OargaTamas
+ * Date: 2019. 01. 27.
+ * Time: 12:41
+ */
+
+namespace MedevSlim\Core\Database;
+
+
+abstract class MedevDatabase
+{
+    //We just need the name of the class to use in the DIC component key (MedevDatabase::class)
+}
