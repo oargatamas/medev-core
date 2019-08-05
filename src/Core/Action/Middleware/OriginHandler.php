068130c374118071578bc6155d8cc8eb54e6e721
Tangomajom <kutyafika>
2019-08-05 07:59:05 +0200

    OriginHandler middleware has been added to the project.

---
+++ src/Core/Action/Middleware/OriginHandler.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: OargaTamas
+ * Date: 2019. 08. 05.
+ * Time: 7:53
+ */
+
+namespace MedevSlim\Core\Action\Middleware;
+
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class OriginHandler
+{
+    private $allowedOrigins;
+
+    /**
+     * OriginHandler constructor.
+     * @param $allowedOrigins
+     */
+    public function __construct($allowedOrigins)
+    {
+        $this->allowedOrigins = $allowedOrigins ?? [];
+    }
+
+
+    public function __invoke(Request $request, Response $response, callable $next)
+    {
+        /** @var Response $finalResponse */
+        $finalResponse = $next($request,$response);
+
+        $origins = count($this->allowedOrigins) == 0 ? "*" : implode(",",$this->allowedOrigins);
+
+        return $finalResponse
+            ->withHeader("Access-Control-Allow-Origin:",$origins);
+    }
+}
