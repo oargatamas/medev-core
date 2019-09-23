f7c8e6891491800b24da272d43e71183e0f46b55
Tangomajom <kutyafika>
2019-09-23 13:17:41 +0200

    Slim router has been added to APITwigRepositoryAction as a property.

--- src/Core/Action/Repository/Twig/APITwigRepositoryAction.php
+++ src/Core/Action/Repository/Twig/APITwigRepositoryAction.php
@@ -10,8 +10,10 @@ namespace MedevSlim\Core\Action\Repository\Twig;
 
 
 use MedevSlim\Core\Action\Repository\APIRepositoryAction;
+use MedevSlim\Core\Application\MedevApp;
 use MedevSlim\Core\Service\View\TwigAPIService;
 use MedevSlim\Core\View\TwigView;
+use Slim\Interfaces\RouterInterface;
 use Slim\Views\Twig;
 
 abstract class APITwigRepositoryAction extends APIRepositoryAction
@@ -22,11 +24,17 @@ abstract class APITwigRepositoryAction extends APIRepositoryAction
     private $view;
 
     /**
+     * @var RouterInterface
+     */
+    protected $router;
+
+    /**
      * @inheritDoc
      */
     public function __construct(TwigAPIService $service)
     {
         $this->view = $service->getContainer()->get(TwigView::class);
+        $this->router = $service->getContainer()->get(MedevApp::ROUTER);
         parent::__construct($service);
     }
 
@@ -35,6 +43,7 @@ abstract class APITwigRepositoryAction extends APIRepositoryAction
      * @param string $template
      * @param array $data
      * @return string
+     * @throws \Twig\Error\LoaderError
      */
     protected function render($template, $data = []){
         return $this->view->fetch("@".$this->service->getServiceName()."/".$template,$data);
